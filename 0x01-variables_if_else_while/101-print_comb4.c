6b4d795bce0d8118398016a0610deb627f74a21b
primePrism <ampah.phil@gmail.com>
2022-10-14 20:33:18 -0700

    do more of what you do

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y, l;
+
+	for (x = 48; x < 58; x++)
+	{
+		for (y = 49; y < 58; y++)
+		{
+			for (l = 50; l < 58; l++)
+			{
+				if (l > y && y > x)
+				{
+					putchar(x);
+					putchar(m);
+					putchar(l);
+					if (x != 55 || y != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
