1311d551d309c2b4fc34cab312bcf5e053d1eff6
primePrism <ampah.phil@gmail.com>
2022-10-27 20:26:33 -0700

    fixed the fourth task

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,18 +6,15 @@
  * @n: number of elements of the array.
  * Return: no return.
  */
-
 void reverse_array(int *a, int n)
 {
-	int i, j, temp;
+	int i;
+	int t;
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n / 2; i++)
 	{
-		for (j = i + 1; j > 0; j--)
-		{
-			temp = *(a + j);
-			*(a + j) = *(a + (j - 1));
-			*(a + (j - 1)) = temp;
-		}
+		t = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = t;
 	}
 }
