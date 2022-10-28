99ac022e80579d0c4b8d247e94d1d46784e4dfd1
primePrism <ampah.phil@gmail.com>
2022-10-27 20:35:42 -0700

    final fix for file 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,13 +8,15 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i;
-	int t;
+	int i, j, temp;
 
-	for (i = 0; i < n / 2; i++)
+	for (i = 0; i < n - 1; i++)
 	{
-		t = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1] = t;
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
 	}
 }
