10a2ac40d2828508b9f779dca1b83e2c86cb0520
primePrism <ampah.phil@gmail.com>
2022-11-18 18:25:31 -0800

    numbers fixed

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,20 +7,22 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int in;
-	va_list args;
+	unsigned int i;
+	va_list list;
 
 	va_start(list, n);
 
-	for (in = 0; in < n; in++)
+	for (i = 0; i < n; i++)
 	{
 		if (!separator)
-			printf("%d\n", va_arg(args, int));
+			printf("%d", va_arg(list, int));
 		else if (separator && i == 0)
-			printf("%d\n", va_arg(args, int));
+			printf("%d", va_arg(list, int));
 		else
-			printf("%s%d\n", separator, va_arg(args, int));
+			printf("%s%d", separator, va_arg(list, int));
 	}
 
-	va_end(args);
+	va_end(list);
+
+	printf("\n");
 }
